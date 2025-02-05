63f2208d8b18852940980b0ce0440c86723a0851
SrushtiAmin <srushtiamin2@gmail.com>
2025-02-05 14:32:56 +0530

    photo

---
+++ src/firebase.jsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBQieY4M2e6eYSgRTbgHEh0kr6YOqYdUSg",
+    authDomain: "quizfizz-c81e3.firebaseapp.com",
+    projectId: "quizfizz-c81e3",
+    storageBucket: "quizfizz-c81e3.firebasestorage.app",
+    messagingSenderId: "947288215865",
+    appId: "1:947288215865:web:5eb2159ee054321fda58e8",
+    measurementId: "G-YYTR51L1ZN"
+  };
+
+  export const app = initializeApp(firebaseConfig) 
